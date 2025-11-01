5e6bdf14583fdbf5871b0f9f71e42e774a584934
ajlalhasan <ajlalhasan03@gmail.com>
2025-11-01 21:25:22 +0500

    purchase_request

--- purchase_request/__manifest__.py
+++ purchase_request/__manifest__.py
@@ -10,7 +10,7 @@
     "requirements.",
     "website": "https://github.com/OCA/purchase-workflow",
     "category": "Purchase Management",
-    "depends": ["purchase"],
+    "depends": ["purchase", "stock"],
     "data": [
         "security/purchase_request.xml",
         "security/ir.model.access.csv",
